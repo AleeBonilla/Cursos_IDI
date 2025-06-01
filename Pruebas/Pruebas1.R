61455a2a146c90e235851200561dfb4b2260ce17
Andres Rodríguez <167501078+AndresRodz01@users.noreply.github.com>
2025-06-01 14:33:27 -0600

    Grafico personalizado

--- Pruebas/Pruebas1.R
+++ Pruebas/Pruebas1.R
@@ -1,8 +1,21 @@
-
 library(visNetwork)
 
-nodes <- read.csv("Nodos y enlaces/nodes.csv", fileEncoding = "latin1", sep = ";", header = TRUE) 
+nodes <- read.csv("Nodes.csv", fileEncoding = "latin1", sep = ";", header = TRUE)
+edges <- read.csv("edges.csv", sep = ";", header = TRUE)
 
-edges <- read.csv("Nodos y enlaces/edges.csv", sep = ";", header = TRUE)
+nodes$área <- as.factor(nodes$área)
+nodes$bloque <- as.factor(nodes$bloque)
 
-visNetwork(nodes, edges)
+area_colores <- rainbow(length(levels(nodes$área)))
+names(area_colores) <- levels(nodes$área)
+
+nodes$color <- area_colores[nodes$área]
+nodes$value <- nodes$creditos  
+nodes$shape <- ifelse(nodes$bloque == "1", "ellipse",
+                      ifelse(nodes$bloque == "2", "box",
+                             ifelse(nodes$bloque == "3", "triangle", "dot")))
+
+# Visualizar el grafo
+visNetwork(nodes, edges) %>%
+  visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE)
+    
