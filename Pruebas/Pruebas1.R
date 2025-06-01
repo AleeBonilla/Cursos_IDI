3c2ab8eccb05fa97d8a0ab95f256d50ed8cfb822
Alee Bonilla <abofi2005@gmail.com>
2025-05-31 20:15:20 -0600

    Grafo listo.

--- Pruebas/Pruebas1.R
+++ Pruebas/Pruebas1.R
@@ -1,3 +1,8 @@
 
+library(visNetwork)
 
-nodes <- read.csv("Nodos y enlaces/Nodes.csv", fileEncoding = "latin1", sep = ";", header = TRUE) 
+nodes <- read.csv("Nodos y enlaces/nodes.csv", fileEncoding = "latin1", sep = ";", header = TRUE) 
+
+edges <- read.csv("Nodos y enlaces/edges.csv", sep = ";", header = TRUE)
+
+visNetwork(nodes, edges)
